9d57cc24b6a485f07924dbac0bdec981c3d1b148
estudante Suelin Aiessa <106682937+suelinaiessa@users.noreply.github.com>
2024-08-12 08:10:30 -0300

    Add files via upload

---
+++ ações.js
@@ -0,0 +1,17 @@
+let resp = window.document.getElementById('saida')
+
+function acao1(){
+    resp.inert += <p>Clicou no primeiro botão</p>
+}
+
+function acao2(){
+    resp.inert += <p>Clicou no segundo botão</p>
+}
+
+function acao3(){
+    resp.inert += <p>Clicou no terceiro botão</p>
+}
+
+function acao4(){
+    resp.inert += <p>Clicou no quarto botão</p>
+}
